b0889438ec3faa5f7a133ace142573051ed1ee33
SantamariaCodes <diego.santamaria75@gmail.com>
2023-12-06 13:32:40 -0600

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Diego Santamaria on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
