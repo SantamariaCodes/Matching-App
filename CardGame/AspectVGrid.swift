37521aeb27fb212ccfb7b6fbb6827ef1a958824f
SantamariaCodes <diego.santamaria75@gmail.com>
2023-12-12 16:28:34 -0600

    lesson 6 updated

---
+++ CardGame/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  CardGame
+//
+//  Created by Diego Santamaria on 12/12/23.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
